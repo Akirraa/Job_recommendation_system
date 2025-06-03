9b874a16b8bab52a451bce110c7f84250ccfee50
Akirraa <rayane.kahlaoui50@gmail.com>
2025-06-03 16:17:03 +0100

    update

--- Applications/views.py
+++ Applications/views.py
@@ -1,8 +1,8 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.decorators import action
+
 
 from .models import Application, Interaction
 from .serializers import ApplicationSerializer, InteractionSerializer
